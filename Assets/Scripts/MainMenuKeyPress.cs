757f6a5efb8fdecd26783079b695ef5113d69295
bet-no-one <evie.evans@ntlworld.com>
2017-01-12 15:25:11 +0000

    first comit of ransack game

---
+++ Assets/Scripts/MainMenuKeyPress.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuKeyPress : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.Space))
+			SceneManager.LoadScene ("Settings");
+		if (Input.GetKey (KeyCode.S))
+			SceneManager.LoadScene ("Settings");
+		if (Input.GetKey (KeyCode.H))
+			SceneManager.LoadScene ("Settings");
+		if (Input.GetKey (KeyCode.Q))
+			Application.Quit ();
+	}
+}
---
+++ Assets/Scripts/SettingsPageKeyPress.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SettingsPageKeyPress : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.A)) {
+			// Toggle Sound setting
+			SceneManager.LoadScene("Settings");
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			// Toggle Difficulty setting
+			SceneManager.LoadScene("Settings");
+			}
+
+		if (Input.GetKey (KeyCode.R)) {
+			// return to main menu
+			SceneManager.LoadScene("MainMenu");
+		}
+	}
+}
